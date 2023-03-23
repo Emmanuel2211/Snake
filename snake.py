e07bc154a22f894a18091d90e8ecd63e6a61bb0c
Leonardo López <A01653931@tec.mx>
2023-03-23 12:55:15 -0600

    Añadí mi nombre

--- snake.py
+++ snake.py
@@ -6,6 +6,8 @@ Exercises
 2. How can you make the snake go around the edges?
 3. How would you move the food?
 4. Change the snake to respond to mouse clicks.
+
+Editado por Leonardo López
 """
 
 from random import randrange
